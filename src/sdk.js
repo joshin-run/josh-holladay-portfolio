56c2542fe3a466b79f676e4b8bdb81a0ba05728b
Josh Holladay <jholladay@nuskin.com>
2021-04-26 16:24:06 -0600

    added sdk content and config

---
+++ src/sdk.js
@@ -0,0 +1,21 @@
+import firebase from "firebase/app";
+import 'firebase/functions';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBpIu4FOqKQgl4IVsdR6Yced7xAjU1RrUU",
+    authDomain: "calico-7c4ba.firebaseapp.com",
+    projectId: "calico-7c4ba",
+    storageBucket: "calico-7c4ba.appspot.com",
+    messagingSenderId: "714349983765",
+    appId: "1:714349983765:web:6269ddd18883a20b59761c"
+};
+
+const app = firebase.initializeApp(firebaseConfig);
+
+const firebaseFunctions = app.functions();
+firebaseFunctions.useEmulator('localhost', 5001);
+
+export async function helloWorld(): Promise<void> {
+  const res = await firebaseFunctions.httpsCallable('helloWorld')({});
+  console.log(res);
+}
